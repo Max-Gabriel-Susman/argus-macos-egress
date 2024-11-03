168d27d01e61fc141c8c9bf1e224952b159443c8
Gabe Susman <gabe.susman@gmail.com>
2024-11-02 23:19:11 -0600

    first commit

---
+++ argus-macos-egress/argus_macos_egressApp.swift
@@ -0,0 +1,17 @@
+//
+//  argus_macos_egressApp.swift
+//  argus-macos-egress
+//
+//  Created by Max Gabriel Susman on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct argus_macos_egressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ argus-macos-egressTests/argus_macos_egressTests.swift
@@ -0,0 +1,17 @@
+//
+//  argus_macos_egressTests.swift
+//  argus-macos-egressTests
+//
+//  Created by Max Gabriel Susman on 11/2/24.
+//
+
+import Testing
+@testable import argus_macos_egress
+
+struct argus_macos_egressTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
